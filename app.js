4d879fffbb784e939c4013979288ca417d8944e7
Gaelpem <pembelegael@gmail.com>
2025-06-07 09:52:52 +0200

    5eme

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const valeurInput = document.getElementById("valeurInput")
 const affichageList  = document.getElementById("affichage"); 
-const boutonAjouter  = document.getElementById("ajouer"); 
+const boutonAjouter  = document.getElementById("ajouterTache"); 
 
- function creerTache(titre, statut = false ) {
+function creerTache(titre, statut = false ) {
     return {
         titre, 
         statut, 
@@ -19,7 +19,7 @@ const gestionnaireTache  = {
     tableauTaches : [], 
     // une methode pour  ajouter les taches dans le tableau 
     addTache : function(tache){
-      this.tableauTaches.addTache(tache); 
+      this.tableauTaches.push(tache); 
     }, 
     // une mehode pour afficher les taches 
     afficheTache : function(){
@@ -27,40 +27,43 @@ const gestionnaireTache  = {
         let self = this ; // Pour garder la référence à gestionTache dans les fonctions internes
     
     // on parcourt chaque element du tableau pour les afficher sous formes de listes 
-      this.tableauTaches.forEach(indexTache, tache => {
+      this.tableauTaches.forEach((tache,indexTache) => {
          
         const li =  document.createElement("li"); 
         li.innerHTML = tache.afficheInfo(); 
     
         const checkbox  = document.createElement("input"); 
         checkbox.type = "checkbox"; 
+        // ici, on initialise la case selon la tâche
+        checkbox.checked = tache.statut;
+
         checkbox.addEventListener('change', function(){
-            
+            tache.statut = checkbox.checked;//Si la case est cochée, le statut devient true. Sinon, false
+            self.save(); // pour sauvegarder le changement 
         })
 
         const suppElement = document.createElement("button"); 
         suppElement.innerHTML= "supprimer"; 
-
         suppElement.addEventListener('click', function(){
           self.tableauTaches.splice(indexTache , 1); 
-          valeurInput.value = ""; 
+          self.save()
+          self.afficheTache()// pour mettre à jour la sauvegarde 
         })
-
-       
         //assemblage
         li.appendChild(checkbox);
         li.appendChild(suppElement); 
-        affichageList.appendChild(li);
-       
+        affichageList.appendChild(li);      
       });
     }, 
 
     save : function(){
-        localStorage.setItem("taches", JSON.parse(this.taches))
+        // stockage des donnees dans le navigateur, même apres un rechargement ou la fermeture de la page 
+        localStorage.setItem("taches", JSON.stringify(this.tableauTaches));
+
     }
 }
 
-boutonAjouter.addEventListener('click', ()=>{
+boutonAjouter.addEventListener('click', function(){
 
     const recupValeur = valeurInput.value.trim(); 
     // onn verifie si une tache existe comme ca on le cree
@@ -73,10 +76,43 @@ boutonAjouter.addEventListener('click', ()=>{
     }
 })
 
-
+// Au chargement de la page, on récupère les tâches stockées
 window.addEventListener('load', function(){
-    let text = JSON.stringify(this.tableauTaches); 
-    localStorage.setItem("taches", text); 
 
+    // on enregistre les données  dans le navigateur
 
+     // ON recupere les taches stockées
+     const  donnees  =  localStorage.getItem("taches");
+   
+    if(donnees) {
+
+   // on transforme les taches(textes) en objets bruts sans methods; 
+   let objetBrut = JSON.parse(donnees); 
+
+     // on reconstruit 
+
+    const gestionnaireObjetTache = {
+
+        tableauTaches : [],
+        // les taches sous formes de propriete
+        bruts : objetBrut, //  
+        afficheTacheObjets : function(){
+            
+           this.bruts.forEach(donnee => {
+                let tache  = creerTache(donnee.titre, donnee.statut)
+                this.ajouterTache(tache)
+             });
+        }, 
+
+        ajouterTache : function(tache){
+            this.tableauTaches.push(tache)
+        }
+        
+     }
+     gestionnaireObjetTache.afficheTacheObjets(); 
+     gestionnaireTache.tableauTaches =  gestionnaireObjetTache.tableauTaches; 
+     gestionnaireTache.afficheTache()
+     
+    }
+   
 })
